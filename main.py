5b9f9fb708a6ec5dd63e636dbd4160839dcf3cd5
felipemonsef10 <felipemonsef10@gmail.com>
2025-08-24 14:54:40 -0300

    criando pagina de usuarios

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_users = [
+    'Felipe',
+    'João',
+    'Fernanda'
+]
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,6 +17,12 @@ def home():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_users=lista_users)
+
 
 
 if __name__ == '__main__':
