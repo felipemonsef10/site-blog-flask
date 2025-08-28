56eacc8e39ee4df3060fa42a6171b158649e266c
felipemonsef10 <felipemonsef10@gmail.com>
2025-08-28 00:04:16 -0300

    Criando os forms de login

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'cafe21d28999850c8855b75bb3192523'
+
 
 lista_users = [
     'Felipe',
@@ -14,15 +18,21 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/contato')
+@app.route('/contatos')
 def contato():
-    return render_template('contato.html')
+    return render_template('contatos.html')
 
 
 @app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_users=lista_users)
 
+
+@app.route('/login')
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
 if __name__ == '__main__':
