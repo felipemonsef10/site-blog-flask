aefee26dc5a48eb8bc65090088a8806c37fc5bbf
felipemonsef10 <felipemonsef10@gmail.com>
2025-09-01 22:39:50 -0300

    formularios validados

--- forms.py
+++ forms.py
@@ -3,16 +3,16 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_login = BooleanField('Lembrar-me')
+    botao_submit_login = SubmitField('Fazer Login')
+
+
 class FormCriarConta(FlaskForm):
     username = StringField('Nome do Usuário', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criar_conta = SubmitField('Criar Conta')
-
-
-class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    lembrar_login = BooleanField('Lembrar-me')
-    botao_submit_login = SubmitField('Fazer Login')
