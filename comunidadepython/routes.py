ef5f7f0377b74fdaa9841efd1ec8bfd8f4627dd4
felipemonsef10 <felipemonsef10@gmail.com>
2025-09-06 18:41:03 -0300

    validando login

--- comunidadepython/routes.py
+++ comunidadepython/routes.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, flash, url_for, request
 from comunidadepython import app, db, bcrypt
 from comunidadepython.forms import FormLogin, FormCriarConta
 from comunidadepython.models import Usuario
+from flask_login import login_user
+
 
 lista_users = [
     'Felipe',
@@ -31,9 +33,14 @@ def login():
     form_criarconta = FormCriarConta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash('Login feito com sucesso. Email: {}'.format(form_login.email.data), 'alert-success')
-        return redirect(url_for('home'))
-    
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_login.data)
+            flash('Login feito com sucesso. Email: {}'.format(form_login.email.data), 'alert-success')
+            return redirect(url_for('home'))
+        else:
+            flash('Falha no Login. E-mail ou senha incorretos', 'alert-danger')
+
     if form_criarconta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
         senha_criptografada = bcrypt.generate_password_hash(form_criarconta.senha.data)
         with app.app_context():
