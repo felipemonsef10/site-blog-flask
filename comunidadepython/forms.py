b62f4d6453b6d3ca44a63d1d2ade68c5f00fd6f3
felipemonsef10 <felipemonsef10@gmail.com>
2025-11-12 22:09:17 -0300

    arrumando a página de editar perfil

--- comunidadepython/forms.py
+++ comunidadepython/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadepython.models import Usuario
+from flask_login import current_user
 
 
 class FormLogin(FlaskForm):
@@ -28,3 +29,9 @@ class FormEditarPerfil(FlaskForm):
     username = StringField('Nome do Usuário', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     botao_submit_editar_perfil = SubmitField('Confirmar')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('E-mail já cadastrado! Escolha outro e-mail.')
