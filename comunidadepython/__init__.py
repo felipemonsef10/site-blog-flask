063cfd4a916ff5cc905ec29ae3692674026744b6
felipemonsef10 <felipemonsef10@gmail.com>
2025-12-23 22:47:22 -0300

    alterando o init

--- comunidadepython/__init__.py
+++ comunidadepython/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'cafe21d28999850c8855b75bb3192523'
+
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
