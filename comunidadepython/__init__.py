0a93c814dd9dd318811fc86e4d4d2da4cdd0c120
felipemonsef10 <felipemonsef10@gmail.com>
2025-12-25 23:00:26 -0300

    adaptando o codigo para o deploy

--- comunidadepython/__init__.py
+++ comunidadepython/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 
 app = Flask(__name__)
@@ -21,5 +22,22 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Faça login para continuar!'
 login_manager.login_message_category = 'alert-warning'
 
+from comunidadepython import models
+
+if 'sqlite' not in app.config['SQLALCHEMY_DATABASE_URI']:
+    engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    inspector = sqlalchemy.inspect(engine)
+
+    if not inspector.has_table('usuario'):
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+        print('-----------------------')
+        print('Database criado')
+        print('-----------------------')
+    else:
+        print('-----------------------')
+        print('Database já existente')
+        print('-----------------------')
 
 from comunidadepython import routes
