f2b4f8020999caab56a88a12b313e82e472006ec
felipemonsef10 <felipemonsef10@gmail.com>
2025-12-29 22:58:52 -0300

    alterando a pagina de contatos

--- comunidadepython/models.py
+++ comunidadepython/models.py
@@ -25,5 +25,5 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String, nullable=False)
     corpo = db.Column(db.Text, nullable=False)
-    data_criacao = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    data_criacao = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
